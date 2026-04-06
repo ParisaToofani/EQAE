090ebbe68889f1e74f3c46c6d0c80458e2fbe291
ptoofani <toofaniparisa@gmail.com>
2026-04-06 17:44:25 -0400

    1- added rundata split scriptor

---
+++ scripts/run_data_split.py
@@ -0,0 +1,8 @@
+from process_raw_data.data_split import main
+
+if __name__ == "__main__":
+    """
+    You need to change the amin function based on your 
+    need at data_split file
+    """
+    main()
